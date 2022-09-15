cd9fd7c2fa470e6bb6094d69097683acf9fd7ac1
vishal1145 <vishal1145@gmail.com>
2022-09-15 18:40:36 +0530

    changes

--- server.js
+++ server.js
@@ -205,10 +205,10 @@ const generatePDF = async (req) => {
 
   try {
     const browser = await puppeteer.launch({
-      // executablePath: '/usr/bin/google-chrome'    //google-chrome-stable
+      executablePath: '/usr/bin/chromium' //google-chrome'    //google-chrome-stable
       // headless: true,
       // args: ['--use-gl=egl'],
-      args: ['--no-sandbox']
+      // args: ['--no-sandbox']
     })
     const page = await browser.newPage()
 
