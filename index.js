03a4f34c66cd391fba99f78eb8b2513686d9a0d8
vishal1145 <vishal1145@gmail.com>
2022-12-05 16:24:02 +0530

    changes

--- index.js
+++ index.js
@@ -647,10 +647,10 @@ const generateAnswerPDF = async (req) => {
     try {
       console.log("Log", "Staring pupeeter");
       const browser = await puppeteer.launch({
-        executablePath: path.resolve(
-          __dirname,
-          "../node_modules/puppeteer/.local-chromium/linux-982053/chrome-linux/chrome"
-        ),
+        // executablePath: path.resolve(
+        //   __dirname,
+        //   "../node_modules/puppeteer/.local-chromium/linux-982053/chrome-linux/chrome"
+        // ),
         // executablePath: "/usr/bin/chromium",
         // executablePath: '/usr/bin/google-chrome-stable',
         // executablePath: '/usr/bin/google-chrome',
