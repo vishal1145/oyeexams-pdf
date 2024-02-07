b8fc2acfb44f68591f5a1284b94d7fc0099b78b3
vishal1145 <vishal.kumar1145@gmail.com>
2024-02-07 13:59:25 +0530

    fixed option

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get("/", (req, res) => {
 function optionDiv(OptionValue, optionSlag , Question) {
 
   let QuestionNumber = Question.QueIndex;
-  let marginLeftItems = QuestionNumber.toString().length === 2 ? "52px" : "45px";
+  let marginLeftItems = QuestionNumber.toString().length === 2 ? "55px" : "48px";
 
   let optionText = `<div class="optiontop" style="margin-left:${marginLeftItems};">
     <span class="optionstyle">$$optionslag$$.&nbsp; </span>$$options$$
